cc743e83df305ecef119871aff6aa599d75e4ac6
Mihir Singh <me@mihirsingh.com>
2013-01-28 23:35:40 -0500

    a lot

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,40 +1,58 @@
 import sys
-from flask import Flask, render_template
+import yaml
+import markdown
+from flask import Flask,render_template
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
-DEBUG = True
-FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = '.md'
+config = yaml.load(open('config.yaml'))
 
-app = Flask(__name__)
+DEBUG = True
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'posts'
+
+app = Flask(__name__,
+            static_folder='themes/'+config['theme']+'/static',
+            template_folder='themes/'+config['theme']+'/templates')
 app.config.from_object(__name__)
-pages = FlatPages(app)
+posts = FlatPages(app)
 freezer = Freezer(app)
+
+@app.template_filter('markdown')
+def md(s):
+    return markdown.markdown(s, extensions=['extra'])
 
 @app.route('/')
 def index():
-    return render_template('index.html', pages=pages)
+    return render_template('index.html', posts=posts, config=config)
 
 @app.route('/tag/<string:tag>/')
 def tag(tag):
-    tagged = [p for p in pages if tag in p.meta.get('tags', [])]
-    return render_template('tag.html', pages=tagged, tag=tag)
+    tagged = [p for p in posts if tag in p.meta.get('tags', [])]
+    return render_template('tag.html', posts=tagged, tag=tag, config=config)
 
 @app.route('/cat/<string:cat>/')
 def cat(cat):
-    filed = [p for p in pages if cat in p.meta.get('category')]
-    return render_template('cat.html', pages=filed, cat=cat)
+    filed = [p for p in posts if cat in p.meta.get('category')]
+    return render_template('cat.html', posts=filed, cat=cat, config=config)
 
+@app.route('/author/<string:author>/')
+def author(author):
+    authored = [p for p in posts if author in p.meta.get('author')]
+    return render_template('author.html', posts=authored, author=author, config=config)
 
 @app.route('/<path:path>/')
-def page(path):
-    page = pages.get_or_404(path)
-    return render_template('page.html', page=page)
-    
-@app.route('/map/')
+def post(path):
+    post = posts.get_or_404(path)
+    return render_template('page.html', post=post, config=config)
+
+@app.route('/404/')
 def fourohfour():
-    return render_template('map.html')
+    return render_template('404.html', config=config)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', config=config)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
